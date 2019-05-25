e58b5af01094d06dfd1c3ae574805d524f89e713
Kant <40262281+Kant1192@users.noreply.github.com>
2019-05-25 20:54:57 +0900

    Update gcd_and_lcm.cpp

--- gcd_and_lcm.cpp
+++ gcd_and_lcm.cpp
@@ -1,17 +1,17 @@
-#define long long ll
+#include<bits/stdc++.h>  
+using namespace std;  
 
-ll gcd(ll a,ll b){
+int gcd(int a,int b){
     if(b == 0) return a;
     return gcd(b,a%b);
 }
 
-ll lcm(ll a,ll b){
-    ll g = gcd(a,b);
+int lcm(int a,int b){
+    int g = gcd(a,b);
     return a*b/g;
 }
 
 /*  
     文字の大小に注意
-
     計算量が多い場合再起関数でなく繰り返し処理の方に適宣変えるべき　
 */
