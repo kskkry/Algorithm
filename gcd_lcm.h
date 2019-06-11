71ed38163fe8a72d07d9a329d4f115ed6028fa59
Kant <40262281+Kant1192@users.noreply.github.com>
2019-06-12 02:44:29 +0900

    Update gcd_lcm.h

--- gcd_lcm.h
+++ gcd_lcm.h
@@ -1,6 +1,3 @@
-#include<bits/stdc++.h>  
-using namespace std;  
-
 int gcd(int a,int b){
     if(b == 0) return a;
     return gcd(b,a%b);
@@ -10,12 +7,6 @@ int lcm(int a,int b){
     int g = gcd(a,b);
     return a*b/g;
 }
-
-/*  
-    文字の大小に注意
-    時にはlong long などへの必要
-    計算量が多い場合再起関数でなくmain関数内での繰り返し処理の方に適宣変えるべき　
-*/
 
 /*short gcd*/
 int GCD(int x,int y){
