860dea33724731b48f9c6929679f4bd7a8275da2
Kant1192 <koskar0358@gmail.com>
2019-07-13 22:01:10 +0900

    add file

---
+++ AtCoder/AGC/a.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <utility>
+#include <stack>
+#include <numeric>
+#include <algorithm>
+#include <stdio.h>
+#include <bitset>
+#include <complex>
+#include <iomanip>
+using namespace std;
+#define MM 1000000000
+#define MOD MM+7
+#define pi pair<int32,int32>
+#define pl pair<int64,int64>
+#define chmax(a,b) (a<b ? a=b:0)
+#define chmin(a,b) (a>b ? a=b:0)
+const long double PI = acos(-1);
+const long long INF = 1e15;
+int dx[8] = {-1,0,1,0,-1,-1,1,1};
+int dy[8] = {0,-1,0,1,-1,1,1,-1};
+template<typename T>
+T GCD(T u,T v){
+    return v ? GCD(v,u%v) : u;
+}
+int main(){
+}
+  
+
