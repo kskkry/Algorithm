45d3d87b9d031a23a0b89a1d3c2387234ffb37b3
Kant1192 <koskar0358@gmail.com>
2019-07-14 20:45:32 +0900

    add template

--- Other/gcd_lcm.cpp
+++ Other/gcd_lcm.cpp
@@ -1,3 +1,12 @@
+template<typename T>
+T GCD(T u,T v){
+    return v ? GCD(v,u%v) : u;
+}
+template<typename T>
+T lcm(T x,T y){
+    return x*y/GCD(x,y);
+}
+
 int gcd(int a,int b){
     if(b == 0) return a;
     return gcd(b,a%b);
@@ -12,3 +21,4 @@ int lcm(int a,int b){
 int GCD(int x,int y){
     return y ? GCD(y,x%y):x;
 }
+
