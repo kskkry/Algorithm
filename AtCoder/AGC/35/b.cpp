7144964f66fbae3e139163260111dfd9f0b508a7
Kant1192 <koskar0358@gmail.com>
2019-07-15 03:31:36 +0900

    default

--- AtCoder/AGC/35/b.cpp
+++ AtCoder/AGC/35/b.cpp
@@ -32,66 +32,6 @@ T lcm(T x,T y){
     return x*y/GCD(x,y);
 }
 int main(){
-    int n,m; cin >> n >> m;
-    pair<bool,bool> G[n+10][n+10]; //first=辺がある,second=もう有向か
-    int a,b,w,edge[n+10]; //矢印をつけていない辺の数
-    fill(edge,edge+n+5,0);
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            G[i][j] = {false,false};
-        }
-    }
-
-
-    for(int i = 0; i < m; i++){
-        cin >> a >> b;
-        a--; b--;
-        G[a][b] = make_pair(true,false);
-        G[b][a] = make_pair(true,false);
-        edge[a]++;
-        edge[b]++;
-    }
-    vector<pair<int,int> > ans(0); //格納
-    for(int i = 0; i < n; i++){
-        if (edge[i] <= 1) continue;
-        else if (edge[i] % 2 == 0){
-            for(int j = 0; j < n; j++){
-                if (G[i][j].first == true && G[i][j].second == false){
-                    ans.push_back({i,j});
-                    G[i][j].first = false;
-                    G[j][i].first = false;
-
-                    G[i][j].second = true;
-                    G[j][i].second = true;
-                    edge[i]--;
-                    edge[j]--;
-                }
-            }
-        } else if (edge[i] % 2 == 1){
-            int cnt = 0;
-            for(int j = 0; j < n; j++){
-                if (G[i][j].first == true && G[i][j].second == false){
-                    cnt++;
-                    ans.push_back({i,j});
-                    G[i][j].first = false;
-                    G[j][i].first = false;
-
-                    G[i][j].second = true;
-                    G[j][i].second = true;
-                    edge[i]--;
-                    edge[j]--;
-                    if (edge[i] == 1) break;
-                }
-            }
-        }
-    }
-    if (ans.size() < m){
-        cout << -1 << endl;
-        return 0;
-    }
-    for(int i = 0; i < ans.size(); i++){
-        cout << ans[i].first +1 << " " << ans[i].second+1 << endl;
-    }
 }
   
 
