9d52e4b7c19c40c9709b44d56551815bed40094b
Kant1192 <koskar0358@gmail.com>
2019-08-13 14:38:56 +0900

    Add Folder

---
+++ AtCoder/ABC/abc138/d.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <utility>
+#include <stack>
+#include <numeric>
+#include <algorithm>
+#include <tuple>
+#include <stdio.h>
+#include <bitset>
+#include <limits.h>
+#include <complex>
+#include <deque>
+#include <iomanip>
+#include <list>
+using namespace std;
+#define pi pair<int32,int32>
+#define pl pair<int64,int64>
+#define chmax(a,b) (a<b ? a=b:0)
+#define chmin(a,b) (a>b ? a=b:0)
+#define en cout << endl //セミコロンつけろ
+//#define MM 1000000000
+//#define MOD MM+7
+const int MM = 1e9;
+const int MOD = MM+7;
+const long double PI = acos(-1);
+const long long INF = 1e15;
+int dx[8] = {-1,0,1,0,-1,-1,1,1};
+int dy[8] = {0,-1,0,1,-1,1,1,-1};
+// 'A' = 65, 'Z' = 90, 'a' = 97, 'z' = 122
+template<typename T>
+T GCD(T u,T v){
+    return v ? GCD(v,u%v) : u;
+}
+template<typename T>
+T LCM(T x,T y){
+    T gc = GCD(x,y);
+    return x*y/gc;
+}
+
+
+int main(){}
