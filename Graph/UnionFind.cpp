34c32e46f47a16f5c5632a03ba03d35665ac0b31
Kant1192 <koskar0358@gmail.com>
2019-08-24 16:41:20 +0900

    ok

--- Graph/UnionFind.cpp
+++ Graph/UnionFind.cpp
@@ -1,67 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-//任意定数
-const int MAX = 1e5;
-
-//親と深さ
-int parent[MAX],depth[MAX];
-
-//初期化
-void init(int n){
-    for(int i = 0; i < n; i++){
-        parent[i] = i;
-        depth[i] = 0;
-    }
-}
-
-//親の探索
-int find(int x){
-    if(parent[x] == x) return x;
-    else return parent[x] = find(parent[x]);
-}
 
 
-//木の深さを減らし計算量を抑える
-void unite(int x,int y){
-    x = find(x);
-    y = find(y);
-    if(x == y) return;
-    else if (depth[x] < depth[y]) parent[x] = y;
-    else if (depth[x] == depth[y]){
-        depth[x]++;
-        parent[y] = x;
-    }
-    else parent[y] = x;
-}
 
-//確認
-bool check(int x,int y){
-    return find(x) == find(y);
-}
-
-//不要な場合あり
-int size(int x){
-    return -parent[find(x)];
-}
-
-#include<vector>
+//ABC120での解法
 struct UnionFind {
-    vector<int> par;
-    UnionFind(int N) : par(N){
-        for(int i = 0; i < N; i++) par[i] = i;
-    }
-    int root(int x){
-        if (par[x] == x) return x;
-        return par[x] = root(par[x]);
-    }
-    void unite(int x,int y){
-        if (root(x) == root(y)) return;
-        par[root(x)] = root(y);
-    }
-    bool same(int x,int y){
-        return root(x) == root(y);
-    }
-    int size(int x){
-        return -par[find(x)];
-    }
+	vector<int> par, rank, cnt;
+	UnionFind(int n) : par(n), rank(n, 0), cnt(n, 1){
+		for(int i=0; i<n; ++i) par[i] = i;
+	}
+ 
+	int find(int x) {
+		if (x == par[x]) return x;
+		return par[x] = find(par[x]);
+	}
+ 
+	void unite(int x, int y) {
+		x = find(x); y = find(y);
+		if (x == y) return;
+ 
+		if (rank[x] < rank[y]) swap(x, y);
+ 
+		par[y] = x;
+		cnt[x] += cnt[y];
+		if (rank[x] == rank[y]) rank[x]++;
+	}
+ 
+	bool same(int x, int y) {
+		return find(x) == find(y);
+	}
+ 
+	int size(int x) {
+		return cnt[find(x)];
+	}
 };
 
+
+int main(){
+    long long N,M; cin >> N >> M;
+    vector<long long> a(M),b(M);
+    vector<long long> ans(M);
+    long long num = N*(N-1)/2;
+
+    for (int i = 0; i < M; i++){
+        cin >> a[i] >> b[i];
+        a[i]--, b[i]--;
+    }
+
+    UnionFind uf(N);
+
+    for (int i = M-1; i >= 0; i--){
+        ans[i] = num;
+        if (!uf.same(a[i],b[i])){
+            num -= (long long)uf.size(a[i])*(long long)uf.size(b[i]);
+            uf.unite(a[i],b[i]);
+        }
+    }
+
+    for (int i = 0; i < M; i++){
+        cout << ans[i] << endl;
+    }
+} 
