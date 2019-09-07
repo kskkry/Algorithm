d81865e1e5b9e8744ec1e385472f65217008a2c1
Kant1192 <koskar0358@gmail.com>
2019-09-08 01:22:34 +0900

    finish preparing

--- AtCoder/AGC/38/b.cpp
+++ AtCoder/AGC/38/b.cpp
@@ -42,3 +42,27 @@ T LCM(T x,T y){
     T gc = GCD(x,y);
     return x*y/gc;
 }
+struct edge {
+    ll to,cost;
+};
+/*
+const int COM_MAX = 500500;
+long long fac[COM_MAX],finv[COM_MAX],inv[COM_MAX];
+void init(){
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for(int i = 2; i < COM_MAX; i++){
+        fac[i] = fac[i-1]*i%MOD;
+        inv[i] = MOD - inv[MOD%i]*(MOD/i)%MOD;
+        finv[i] = finv[i-1]*inv[i]%MOD;
+    }
+}
+long long COM(int n,int k){
+    if(n < k || n < 0 || k < 0) return 0;
+    return fac[n]*(finv[k]*finv[n-k]%MOD)%MOD;
+}
+*/
+
+int main(){
+}
