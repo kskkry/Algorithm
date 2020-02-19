e826e64f8e03b9c79cce9651be5cd35f89786ef7
KryKsk <koskar0358@gmail.com>
2020-02-20 00:54:32 +0900

    改良（？）済み

--- Graph/TreeDiameter.cpp
+++ Graph/TreeDiameter.cpp
@@ -3,37 +3,79 @@
 #include <iostream>
 using namespace std;
 
+
+
+
+// AC : http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_5_A&lang=jp
+struct Edge
+{
+    int to, cost;
+};
+
 vector<bool> visited;
-vector<vector<int> > g;
+vector<vector<Edge> > g;
 vector<int> dist;
 
 // 'N'を頂点数として...
-pair<int, int> DFS_for_TreeDiameter(int now, int from, vector<vector<int> > &G, int maxDist, int maxDistPos){
-    for (int i = 0; i < G[now].size(); i++){
-        int to = G[now][i];
-        if (to == from) continue; //逆流を防ぐ
-        if (dist[to] == 0){
-            dist[to] = dist[now] + 1;
-            if (dist[to] > maxDist){
-                maxDist = dist[to];
-                maxDistPos = to;
-            }
-            DFS_for_TreeDiameter(to, now, G, maxDistPos);
-        }
+void DFS_for_TreeDiameter(int now, int from, vector<vector<Edge> > &G, int &maxDist, int &maxDistPos){
+    visited[now] = true;
+    if (dist[now] > maxDist){
+        maxDist = dist[now];
+        maxDistPos = now;
     }
-    pair<int,int> re = {maxDist, maxDistPos};
-    return re;
+    for (int i = 0; i < G[now].size(); i++){
+        int To = G[now][i].to;
+        if (To == from){
+            continue;
+        }
+        dist[To] = dist[now] + G[now][i].cost;
+        DFS_for_TreeDiameter(To, now, G, maxDist, maxDistPos);
+    }
 }
 
 
-void Implement(){
-    int start = 0, fin = 0, maxDistPos = 0, maxDist = 0;
+struct treeDiameterInfo
+{
+    int start, end, maxDist;
+};
+
+//始点、終点、最大距離を返す
+treeDiameterInfo Implement(int N, vector<vector<Edge> > &G){
+    int st = 0, fin = 0, maxDistPos = 0, maxDist = 0;
+    visited.assign(N, false);
+    dist.assign(N, 0);
+    //始点情報を入手
+    DFS_for_TreeDiameter(st, -1, G, maxDist, maxDistPos);
+    //
+    st = maxDistPos;
+    //cout << "maxDistPos : " << maxDistPos << endl;
+    maxDist = 0;
+    maxDistPos = 0;
+    visited.assign(N, false);
+    dist.assign(N, 0);
+    //終点情報を入手
+    DFS_for_TreeDiameter(st, -1, G, maxDist, maxDistPos);
+    
+    treeDiameterInfo res;
+    res.start = st, res.end = maxDistPos, res.maxDist = maxDist;
+    return res;
 }
 
 int main(){
     int N; cin >> N;
     g.resize(N);
     dist.assign(N, 0);
+    visited.assign(N, false);
+    for (int i = 0; i < N-1; i++){
+        int s, t, w; cin >> s >> t >> w;
+        Edge e; e.to = t; e.cost = w;
+        g[s].push_back(e);
+        //無向グラフ
+        e.to = s;
+        g[t].push_back(e);
+    }
+    treeDiameterInfo result = Implement(N, g);
+    cout << result.maxDist << endl;
 }
 
 
