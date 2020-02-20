e9d413eec467542b073c006001427e54e411ba76
KryKsk <koskar0358@gmail.com>
2020-02-20 22:36:22 +0900

    segtreeの表示を変えた

--- DataStructure/RMQ.cpp
+++ DataStructure/RMQ.cpp
@@ -21,8 +21,7 @@ using namespace std;
 #define chmax(a,b) (a<b ? a=b:0)
 #define chmin(a,b) (a>b ? a=b:0)
 #define en cout << endl //セミコロンつけろ
-//#define MM 1000000000
-//#define MOD MM+7
+typedef long long ll;
 const int MM = 1e9;
 const int MOD = MM+7;
 const long double PI = acos(-1);
@@ -41,119 +40,72 @@ T LCM(T x,T y){
     return x*y/gc;
 }
 
-//RMQ verify
-int n,q; 
-const int MAX = 1 << 17;
-int dat[MAX*2];
-void SegT(){
-    int tmp = 1;
-    while (tmp < n) tmp *= 2;
-    n = tmp;
-    for (int i = 0; i < n*2-1; i++){
-        dat[i] = INT_MAX;
-    }
-}
-void update(int k,int x){
-    k += n-1;
-    dat[k] = x;
-    while (k > 0){
-        k = (k-1)/2;
-        dat[k] = min(dat[k*2 + 1], dat[k*2 + 2]);
-    }
-}
 
-int find(int a,int b,int k,int l,int r){
-    if (r <= a || b <= l) return INT_MAX;
-    if (a <= l && r <= b) return dat[k];
-    int v1 = find(a,b,k*2 + 1, l, (l+r)/2);
-    int v2 = find(a,b,k*2 + 2, (l+r)/2, r);
-    return min(v1,v2);
-}
 
-int main(){
-    cin >> n >> q;
-    SegT();
-    for (int i = 0; i < q; i++){
-        int c,x,y; cin >> c >> x >> y;
-        if (c == 0) {
-            update(x,y);
-        } else {
-            cout << find(x,y+1,0,0,n) << endl;
+struct SegTree
+{
+    vector<int> pos;
+    int limit;
+
+
+    void Init(int _N){
+        limit = 1;
+        while (limit < _N){
+            limit *= 2;
+        }
+
+        pos.resize(limit*2+1);
+        for (int i = 0; i < limit*2-1; i++){
+            pos[i] = MM;
         }
     }
-}
 
-
-
-
-//ほぼRMQ
-template< typename Monoid >
-struct SegmentTree
-{
-  using F = function< Monoid(Monoid, Monoid) >;
- 
-  int sz;
-  vector< Monoid > seg;
- 
-  const F f;
-  const Monoid M1;
- 
-  SegmentTree(int n, const F f, const Monoid &M1) : f(f), M1(M1)
-  {
-    sz = 1;
-    while(sz < n) sz <<= 1;
-    seg.assign(2 * sz, M1);
-  }
- 
-  void set(int k, const Monoid &x)
-  {
-    seg[k + sz] = x;
-  }
- 
-  void build()
-  {
-    for(int k = sz - 1; k > 0; k--) {
-      seg[k] = f(seg[2 * k + 0], seg[2 * k + 1]);
+    //index番目の値をnewNumに変更
+    void Update(int index, int newNum){
+        index += limit-1;
+        pos[index] = newNum;
+        while (index > 0){
+            index = (index-1)/2;
+            pos[index] = min(pos[index*2 + 1], pos[index*2 + 2]);
+        }
     }
-  }
- 
-  void update(int k, const Monoid &x)
-  {
-    k += sz;
-    seg[k] = x;
-    while(k >>= 1) {
-      seg[k] = f(seg[2 * k + 0], seg[2 * k + 1]);
+
+    //[givenLeft, givenRight]の最小値を求める
+    int Query(int givenLeft, int givenRight, int now, int left, int right){
+        if (right <= givenLeft || givenRight <= left) return MM;
+
+        if (givenLeft <= left && right <= givenRight){
+            return pos[now];
+        } else {
+            int leftUpdate = Query(givenLeft, givenRight, now*2 + 1, left, (left + right)/2);
+            int rightUpdate = Query(givenLeft, givenRight, now*2 + 2, (left + right)/2, right);
+            return min(leftUpdate, rightUpdate);
+        }
     }
-  }
- 
-  Monoid query(int a, int b)
-  {
-    Monoid L = M1, R = M1;
-    for(a += sz, b += sz; a < b; a >>= 1, b >>= 1) {
-      if(a & 1) L = f(L, seg[a++]);
-      if(b & 1) R = f(seg[--b], R);
-    }
-    return f(L, R);
-  }
- 
-  Monoid operator[](const int &k) const
-  {
-    return seg[k + sz];
-  }
 };
 
-//区間の最小値がほしいとき
-SegmentTree< int > seg(n, [](int a, int b){ return min(a, b); }, INT_MAX);
 
-//区間の合計が欲しいとき
-SegmentTree< int > seg(n, [](int a, int b){ return a + b; }, 0);
-
-
-
-
-
-
-
-
-
-
+//Verify "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_A&lang=jp"
+int main(){
+    int N, Q; cin >> N >> Q;
+    const ll MAX = pow(2, 31)-1;
+    SegTree segtree;
+    segtree.Init(N);
+    vector<ll> ans(0);
+    for (int i = 0; i < Q; i++){
+        int c, x, y; cin >> c >> x >> y;
+        if (c == 0){
+            segtree.Update(x, y);
+        } else {
+            int res = segtree.Query(x, y+1, 0, 0, segtree.limit);
+            if (res == MM){
+                ans.push_back(MAX);
+            } else {
+                ans.push_back(res);
+            }
+        }
+    }
+    for (int i = 0; i < ans.size(); i++){
+        cout << ans[i] << endl;
+    }
+}
